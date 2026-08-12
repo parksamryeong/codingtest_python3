d0fa9d8bc60f74cf46a771470485bc241597e069
박삼령 <97231663+parksamryeong@users.noreply.github.com>
2026-08-12 22:59:51 +0900

    [제출] 소수 찾기

---
+++ programmers/연습문제/[Lv1][12921] 소수 찾기/solution.py
@@ -0,0 +1,13 @@
+import math
+
+def solution(n):
+    
+    prime = [True] * (n + 1)
+    prime[0] = prime[1] = False
+    
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if prime[i]:
+            for j in range(i * i, n + 1, i):
+                prime[j] = False
+        
+    return sum(prime)
