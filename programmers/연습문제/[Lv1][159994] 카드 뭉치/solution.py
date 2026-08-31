b7cb1f7422096223ce41a0e14745b31362388237
박삼령 <97231663+parksamryeong@users.noreply.github.com>
2026-08-31 18:13:10 +0900

    [제출] 카드 뭉치

---
+++ programmers/연습문제/[Lv1][159994] 카드 뭉치/solution.py
@@ -0,0 +1,14 @@
+def solution(cards1, cards2, goal):
+    i, j = 0,0
+    
+    for g in goal:
+        if i < len(cards1) and cards1[i] == g:
+            i += 1
+            
+        elif j < len(cards2) and cards2[j] == g:
+            j += 1
+            
+        else:
+            return "No"
+    
+    return "Yes"
